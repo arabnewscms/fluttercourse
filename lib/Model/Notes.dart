ad772be8846bd061782fc31c1dcfbdcd92f34cd9
php anonymous <php.anonymous1@gmail.com>
2019-07-08 05:47:08 +0200

    Add files via upload

---
+++ lib/Model/Notes.dart
@@ -0,0 +1,42 @@
+import 'package:sqflite/sqflite.dart';
+import 'dart:async';
+import 'dart:io' as io;
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+class Notes{
+  String table = 'notes';
+  static Database _db;
+
+  Future<Database> get db async{
+    if(_db == null){
+      _db = await initialDB();
+      return _db;
+    }else{
+      return _db;
+    }
+  }
+
+  initialDB() async{
+    io.Directory docDirect = await getApplicationDocumentsDirectory();
+    String path = join(docDirect.path,'testdb.db');
+    var mydb = await openDatabase(path,version: 1,onCreate: (Database db,int version) async{
+      await db.execute('CREATE TABLE "$table" ("id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,"note"	TEXT NOT NULL)');
+      print('Notes Table Created');
+    });
+    return mydb;
+  }
+
+  Future<int> create(Map<String,dynamic> data) async{
+    var dbclient = await db;
+    int insert = await dbclient.insert(table, data);
+    return insert;
+  }
+
+  Future<List> getData() async{
+    var dbclient = await db;
+    return await dbclient.query(table);
+  }
+
+
+
+}
---
+++ lib/ShowAllNotes.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'Model/Notes.dart';
+class ShowAllNotes extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return ShowAllNotesState();
+  }
+
+}
+
+class ShowAllNotesState extends State<ShowAllNotes>{
+  final GlobalKey<FormState> formState = GlobalKey<FormState>();
+  List data = new List();
+
+  void loadData() async{
+    Notes().getData().then((result){
+      setState(() {
+        data.addAll(result);
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    loadData();
+    super.initState();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Show Notes'),
+      ),
+      body: showData()
+    );
+  }
+
+
+  Widget showData(){
+   if(data != null && data.length > 0) {
+     return ListView.builder(
+         itemCount: data.length,
+         itemBuilder: (context, index) {
+           return ListTile(
+             title: Text('${data[index]['note']}'),
+             onTap: () {
+               //data[index]['id']
+             },
+           );
+         }
+     );
+   }else if(data == null){
+     return Text('No Data At this time');
+   }
+  }
+
+
+}
