f9e12743b8f0dba6876e01f5c52de8781c5b97e6
php anonymous <php.anonymous1@gmail.com>
2019-07-08 05:42:52 +0200

    Add files via upload

---
+++ lib/AddNotes.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'Model/Notes.dart';
+class AddNotes extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return AddNotesState();
+  }
+
+}
+
+class AddNotesState extends State<AddNotes>{
+  final GlobalKey<FormState> formState = GlobalKey<FormState>();
+    String notes;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add Notes'),
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Form(
+              key: formState,
+              child: Container(
+                child: Column(
+                  children: <Widget>[
+                    TextFormField(
+                      maxLines: null,
+                      textAlign: TextAlign.left,
+                      decoration: InputDecoration(
+                        alignLabelWithHint: true,
+                        counterText: 'Enter Notes Unlimted Data',
+                        suffixText: 'Your Notes'
+                      ),
+                      onSaved: (val)=> notes = val,
+                      validator: (val){
+                        if(val.isEmpty){
+                          return 'Please Enter data';
+                        }
+                      },
+                    ),
+                    FlatButton(
+                      child: Text('Add Notes'),
+                      onPressed: addNotes,
+                      color: Colors.green,
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+
+  addNotes() async{
+    if(formState.currentState.validate()){
+      formState.currentState.save();
+     // Notes().create();
+      int add = await Notes().create({
+        'note':'$notes'
+      });
+      print(add);
+    }
+  }
+
+}
